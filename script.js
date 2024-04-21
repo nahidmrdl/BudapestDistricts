3ab23f8bd4eb570ecf6d2a74d2482f68446cce47
Nahid Muradli <124151572+nahidmrdl@users.noreply.github.com>
2024-04-21 23:00:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,165 @@
+const districtDetails = {
+    'district1': {
+        Title: "District 1",
+        id: "district1",
+        description: "District I (Castle District): Explore Budapest's historic heart, where the iconic Buda Castle stands majestically overlooking the Danube River. Wander through charming cobblestone streets lined with medieval architecture and discover cultural treasures like Matthias Church and Fisherman's Bastion.",
+    },
+    'district2': {
+        Title: "District 2",
+        id: "district2",
+        description: "District II (Rózsadomb): Experience the tranquil beauty of District II, nestled among leafy hills and scenic landscapes. This upscale residential area offers stunning panoramas of the city and boasts elegant villas, serene parks, and the lush forests of the Buda Hills for outdoor enthusiasts to enjoy.",
+    },
+    'district3': {
+        Title: "District 3",
+        id: "district3",
+        description: "District III (Óbuda-Békásmegyer): Immerse yourself in the rich heritage of Óbuda, one of Budapest's oldest districts, where ancient Roman ruins blend seamlessly with modern amenities. Explore quaint streets dotted with historic landmarks, vibrant markets, and the picturesque banks of the Danube.",
+        src: "https://welovebudapest.com/hely/obuda-bekasmegyer/",
+    },
+    'district4': {
+        Title: "District 4",
+        id: "district4",
+        description: "District IV (Újpest): Discover the dynamic energy of District IV, a bustling urban hub situated along the northern banks of the Danube. Home to a diverse population and vibrant cultural scene, this district offers eclectic dining options, lively entertainment venues, and scenic waterfront promenades.",
+    },
+    'district5': {
+        Title: "District 5",
+        id: "district5",
+        description: "District V (Belváros-Lipótváros): Delight in the elegance and charm of Budapest's city center, where District V's Belváros-Lipótváros beckons with its grand boulevards, iconic landmarks, and bustling squares. From the majestic Hungarian Parliament Building to the bustling shopping streets of Váci Utca, there's something for everyone in this vibrant district.",
+    },
+    'district6': {
+        Title: "District 6",
+        id: "district6",
+        description: "District VI (Terézváros): Experience the eclectic charm of District VI, where historic architecture blends seamlessly with trendy cafes, boutiques, and cultural institutions. Stroll along the iconic Andrássy Avenue, explore the vibrant nightlife of the Jewish Quarter, or unwind in one of the district's many leafy parks.",
+    },
+    'district7': {
+        Title: "District 7",
+        id: "district7",
+        description: "District VII (Erzsébetváros): Dive into the bohemian spirit of District VII, Budapest's eclectic Jewish Quarter, where street art, ruin bars, and hip cafes converge. Explore the district's rich cultural heritage, from the historic Dohány Street Synagogue to the bustling Gozsdu Courtyard, and experience its vibrant nightlife scene.",
+    },
+    'district8': {
+        Title: "District 8",
+        id: "district8",
+        description: "District VIII (Józsefváros): Delve into the fascinating history of District VIII, where grandiose palaces stand alongside charming courtyards and lively marketplaces. Discover architectural gems like the magnificent Semmelweis Medical University and the iconic New York Café, and soak in the district's unique blend of old-world charm and modern vitality.",
+    },
+    'district9': {
+        Title: "District 9",
+        id: "district9",
+        description: "District IX (Ferencváros): Experience the vibrant atmosphere of Budapest's District IX, a lively neighborhood where historic landmarks and contemporary developments coexist harmoniously. Explore the bustling Central Market Hall, stroll along the picturesque Danube Promenade, and admire the district's eclectic mix of architectural styles.",
+    },
+    'district10': {
+        Title: "District 10",
+        id: "district10",
+        description: "District X (Kőbánya): Uncover the hidden gems of District X, a diverse and dynamic area known for its industrial heritage and cultural landmarks. From the historic Kőbánya breweries to the sprawling City Park, this district offers a wealth of attractions for history buffs, nature lovers, and urban explorers alike.",
+    },
+    'district11': {
+        Title: "District 11",
+        id: "district11",
+        description: "District XI (Újbuda): Embrace the charm of District XI, a residential area brimming with parks, gardens, and recreational facilities along the western bank of the Danube. Explore the serene landscapes of Gellért Hill and Sas Hill, visit the historic Gellért Thermal Bath, and discover the district's vibrant cultural scene.",
+    },
+    'district12': {
+        Title: "District 12",
+        id: "district12",
+        description: "District XII (Hegyvidék): Retreat to the peaceful surroundings of District XII, where verdant hills and lush forests offer a tranquil escape from the bustle of the city. Explore the scenic trails of Normafa and Hármashatár Hill, visit the iconic Elizabeth Lookout Tower, and experience the district's natural beauty and outdoor recreational opportunities.",
+    },
+    'district13': {
+        Title: "District 13",
+        id: "district13",
+        description: "District XIII (Angyalföld-Újlipótváros-Vizafogó): Immerse yourself in the lively ambiance of District XIII, a dynamic area characterized by its diverse communities and bustling urban life. Explore the vibrant waterfront of the Danube, enjoy leisurely strolls along the Margaret Island, and discover the district's eclectic mix of cultural attractions and entertainment venues.",
+    },
+    'district14': {
+        Title: "District 14",
+        id: "district14",
+        description: "District XIV (Zugló): Discover the green oasis of District XIV, a charming residential area renowned for its parks, gardens, and cultural landmarks. Explore the sprawling City Park with its iconic Heroes' Square and Vajdahunyad Castle, wander through the botanical wonders of the Budapest Zoo, and experience the district's vibrant cultural scene.",
+        src: "https://ligetbudapest.hu/storage/5265/conversions/LIGET-BUDAPEST_Palk%C3%B3-Gy%C3%B6rgy_NEP0830-8-%282%29-responsive.webp",
+    },
+    'district15': {
+        Title: "District 15",
+        id: "district15",
+        description: "District XV (Rákospalota-Pestújhely-Újpalota): Experience the authentic charm of District XV, a diverse and vibrant area located in the northern part of Budapest. Explore the district's historic landmarks, lively markets, and green spaces, and immerse yourself in its rich cultural heritage and welcoming community atmosphere.",
+        src: "https://imgcy.trivago.com/c_limit,d_dummy.jpeg,f_auto,h_600,q_auto,w_600//hotelier-images/98/d5/af023374def5623073e9f1c6a7bdf95761a36b1f0a83bba6ff2b16a1cf94.jpeg",
+    },
+    'district16': {
+        Title: "District 16",
+        id: "district16",
+        description: "District XVI (Törökbálint): Escape to the serene countryside surroundings of District XVI, a peaceful residential area located on the outskirts of Budapest. Enjoy leisurely walks along the scenic landscapes of the Budaörs Stream, explore the historic sites of Törökbálint, and experience the district's laid-back atmosphere and rural charm.",
+        src: "https://www.futas.net/terkep/budapest/budapest-xvi-kerulet-terkep.php",
+    },
+    'district17': {
+        Title: "District 17",
+        id: "district17",
+        description: "District XVII (Rákosmente): Explore the suburban charm of District XVII, a tranquil residential area situated on the eastern outskirts of Budapest. Discover the district's lush parks, picturesque lakes, and recreational facilities, and enjoy outdoor activities such as cycling, hiking, and birdwatching in its idyllic natural surroundings.",
+        src: "https://upload.wikimedia.org/wikipedia/commons/b/b3/R%C3%A1koskereszt%C3%BAr_v%C3%A1rosk%C3%B6zpont.jpg",
+    },
+    'district18': {
+        Title: "District 18",
+        id: "district18",
+        description: "District XVIII (Pestszentlőrinc-Pestszentimre): Experience the relaxed ambiance of District XVIII, a peaceful residential area located in the southeastern part of Budapest. Explore the district's charming neighborhoods, verdant parks, and cultural landmarks, and enjoy a leisurely stroll along the scenic banks of the Ráckeve Danube.",
+    },
+    'district19': {
+        Title: "District 19",
+        id: "district19",
+        description: "District XIX (Kispest): Discover the rich history and vibrant culture of District XIX, a dynamic area located in the southern part of Budapest. Explore the district's historic landmarks, bustling markets, and green spaces, and experience its diverse community atmosphere and welcoming hospitality.",
+    },
+    'district20': {
+        Title: "District 20",
+        id: "district20",
+        description: "District XX (Pesterzsébet): Immerse yourself in the authentic charm of District XX, a vibrant residential area located in the southern part of Budapest. Explore the district's historic sites, lively markets, and cultural landmarks, and enjoy leisurely walks along its picturesque streets and tranquil parks.",
+        src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fhu.wikipedia.org%2Fwiki%2FBudapest_XX._ker%25C3%25BClete&psig=AOvVaw2maU0A-g3cMKOoonTSuNht&ust=1713810499315000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCODjiOL304UDFQAAAAAdAAAAABAE",
+    },
+    'district21': {
+        Title: "District 21",
+        id: "district21",
+        description: "District XXI (Csepel): Experience the industrial heritage and cultural diversity of District XXI, a dynamic area located on Csepel Island in the southern part of Budapest. Explore the district's historic factories, bustling markets, and scenic waterfront, and discover its vibrant community atmosphere and rich cultural traditions.",
+        src:"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwelovebudapest.com%2Fi%2F08%2Fcsepel-230224-majorkata-49.jpg&tbnid=H81xa9KfWx_mJM&vet=12ahUKEwiOncWU-NOFAxX48wIHHWYnCOsQMygAegQIARBO..i&imgrefurl=https%3A%2F%2Fwelovebudapest.com%2Fcikk%2F2023%2F02%2F28%2Flatnivalok-es-kultura-az-elet-csepelen-kepgaleria-fotok-budapest%2F&docid=S5hDgzq8w3vw3M&w=3000&h=2000&q=csepel&hl=hu&ved=2ahUKEwiOncWU-NOFAxX48wIHHWYnCOsQMygAegQIARBO",
+    },
+    'district22': {
+        Title: "District 22",
+        id: "district22",
+        description: "District XXII (Budafok-Tétény): Discover the scenic beauty and cultural richness of District XXII, a charming area located in the southwestern part of Budapest. Explore the district's historic wine cellars, verdant vineyards, and cultural landmarks, and enjoy panoramic views of the Danube River and the Buda Hills from its picturesque hillsides.",
+        src:"https://www.google.com/url?sa=i&url=https%3A%2F%2Findex.hu%2Furbanista%2F2016%2F08%2F16%2Fmilyennek_kellene_latnunk_a_torley-_mauzoleumot_az_egykori_pompajaban%2F&psig=AOvVaw0zDF79CiLXIJ7xJVzDG17v&ust=1713810676929000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNjD17b404UDFQAAAAAdAAAAABAH",
+    },
+    'district23': {
+        Title: "District 23",
+        id: "district23",
+        description: "District XXIII (Soroksár): Retreat to the tranquil surroundings of District XXIII, a peaceful residential area located in the southeastern part of Budapest. Explore the district's lush parks, scenic waterfront, and cultural landmarks, and enjoy outdoor activities such as cycling, boating, and picnicking in its idyllic natural setting.",
+        src: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwelovebudapest.com%2Fhely%2Fsoroksari-botanikus-kert%2F&psig=AOvVaw2WF-xR0PWzrm4iVjSNs5fV&ust=1713810854957000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCJCFxYv504UDFQAAAAAdAAAAABAa",
+    },
+
+}
+
+
+const button = document.getElementById('surprise');
+button.addEventListener('click', function() {
+    $.fancybox.open({
+        src: 'images/jamil.jpeg',
+        type: 'image',
+        caption: function(instance, item) {
+            return '<div class="fancybox-caption">' +
+                '<h3>Surprise!</h3>' +
+                '</div>';
+        }
+    });
+});
+
+var shapes = document.querySelectorAll('svg path, svg polygon, svg text, svg polyline');
+
+
+shapes.forEach(function(shape) {
+    shape.addEventListener('click', function() {
+        var id = this.getAttribute('id');
+
+        var imageUrl = 'images/'+ id + '.jpg';  
+        console.log(imageUrl);
+        var title = districtDetails[id].Title;
+        var description = districtDetails[id].description;
+        $.fancybox.open({
+            src: imageUrl,
+            type: 'image',
+            caption: function(instance, item) {
+                return '<div class="fancybox-caption">' + 
+                       '<h3>' + title + '</h3>' +
+                       '<p>' + description + '</p>' +
+                       '</div>';
+            }
+        });
+    });
+});
